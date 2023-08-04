dda4f17cc03ccadd80a99b09891db0a963aef8c9
Joshua Ow <joshuaow@gmail.com>
2023-08-04 18:01:15 -0500

    start, pause, resume button with countdown

--- src/app/box/box.component.ts
+++ src/app/box/box.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Subject, BehaviorSubject, timer, Observable } from 'rxjs';
+import { filter, takeUntil, switchMap, startWith, scan, map, take } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-box',
   templateUrl: './box.component.html',
-  styleUrls: ['./box.component.css']
 })
-export class BoxComponent {
+export class BoxComponent implements OnInit {
+  toggle$ = new BehaviorSubject<boolean>(false);
+  countdown$!: Observable<number>;
+  countdownValue = 4;
+  buttonText = "Start";  
 
+
+  ngOnInit() {
+    this.countdown$ = this.toggle$.pipe(
+      switchMap(isPlaying => isPlaying ? timer(0, 1000) : []),
+      map(() => -1),
+      startWith(this.countdownValue),
+      scan((acc, curr) => {
+        const newValue = acc + curr;
+        if (newValue < 0) {
+          this.buttonText = "Start";
+          this.toggle$.next(false);
+          return this.countdownValue;
+        } else {
+          return newValue;
+        }
+      }),
+    );
+  }
+
+  onToggle() {
+    this.toggle$.next(!this.toggle$.value);
+    switch(this.buttonText) {
+      case "Start":
+        this.buttonText = "Pause";
+        break;
+      case "Pause":
+        this.buttonText = "Resume";
+        break;
+      case "Resume":
+        this.buttonText = "Pause";
+        break;
+    }
+  }
 }
